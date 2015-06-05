840ef793c5820a745c0820f67ab92ff21ef54359
Connor Newton <connor@phyrworks.co.uk>
2015-06-05 21:50:32 +0100

    Fixed a pretty damning encoding error. Note to self: don't test against data from other implementations which haven't been thoroughly tested.
    Also do a non-destructive write during .get() so the code can be extended after a completion if desired.

--- cobss.m
+++ cobss.m
@@ -1,10 +1,10 @@
 classdef cobss < handle
-    %COBSS Consistent Overhead Byte Stuffing Streamer
+    %COBSS Consistent Overhead Byte Stuffing Streaming encoder
     % Byte-wise (streaming) COBS encode - see
     % http://www.stuartcheshire.org/papers/cobsforton.pdf for algorithm
     % details
     
-    properties
+    properties (Access = private)
         data = uint8(1);
         index = 1; % index where next code value (distance to next '0') will go
         count = 1; % next code value (incremented at every non-'0' push)
@@ -17,24 +17,26 @@ classdef cobss < handle
                     obj.data(obj.index) = obj.count; % write stuffing code to start of block
                     obj.count = 1; % reset stuffing code
                     obj.index = length(obj.data) + 1; % mark location of next stuffing code
+                    obj.data(obj.index) = 0; % placeholder
                 otherwise
                     obj.data(end+1) = data; % push data to output
                     obj.count = obj.count + 1; % increment distance to next 0
-                    if obj.count > 255 % max stuffing code value - indicate 254 characters followed by the next code
+                    if obj.count == 255 % max stuffing code value - indicate 254 characters followed by the next code
                         obj.data(obj.index) = 255;
                         obj.count = 1;
-                        obj.index = length(obj.data);
+                        obj.index = length(obj.data) + 1; % mark location of next stuffing code
+                        obj.data(obj.index) = 0; % placeholder
                     end
             end
         end
         
         function data = get(obj)
-            if obj.count == 1; % if last data was a 0
-                obj.data(end+1) = 1; % add a phantom '0' so that the decoder doesn't remove a real one
+            data = obj.data; % don't modify the write-set
+            if obj.data(end) == 1; % if last data was a 0
+                data(end+1) = 1; % add a phantom '0' so that the decoder doesn't remove a real one
             else
-                obj.data(obj.index) = obj.count; % complete last block
+                data(obj.index) = obj.count; % complete last block
             end
-            data = obj.data;
         end
     end  
 end
